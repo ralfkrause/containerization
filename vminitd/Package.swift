5d2d7a1bc32666d163ac7b50bb1a899027bdf39d
Michael Crosby <michael_crosby@apple.com>
2025-06-12 09:57:35 -0400

    update license header removing new line (#99)
    Fixes #63
    
    Signed-off-by: crosbymichael <michael_crosby@apple.com>

--- vminitd/Package.swift
+++ vminitd/Package.swift
@@ -1,7 +1,6 @@
 // swift-tools-version: 6.0
 //===----------------------------------------------------------------------===//
-// Copyright © 2025 Apple Inc. and the Containerization project authors.
-// All rights reserved.
+// Copyright © 2025 Apple Inc. and the Containerization project authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
